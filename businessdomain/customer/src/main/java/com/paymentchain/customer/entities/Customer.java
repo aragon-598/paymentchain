d112f14177e559a283e6151de941bac12cb548fc
Alejandro Aragon <aragondru155@gmail.com>
2023-01-08 12:39:07 -0600

    defining relationships between entities

--- businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -1,11 +1,16 @@
 package com.paymentchain.customer.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -15,6 +20,14 @@ public class Customer implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO  )
     private long id;
-    private String name;
+    private String code;
+    private String Iban;
+    private String names;
+    private String surname;
     private String phone;
+    private String address;
+    @OneToMany(fetch=FetchType.LAZY, mappedBy = "customer",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CustomerProduct> products;
+    @Transient
+    private List<?> transactions;
 }
