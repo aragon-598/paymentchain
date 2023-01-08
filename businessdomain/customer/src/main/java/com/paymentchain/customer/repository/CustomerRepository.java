860a34cc2cd276d77fc50f70fd5427cf2524efbe
Alejandro Aragon <aragondru155@gmail.com>
2023-01-08 12:24:15 -0600

    adding new module project and some changes more

--- businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -1,11 +1,11 @@
 package com.paymentchain.customer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import com.paymentchain.customer.entities.Customer;
 
-@Repository
+//@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
     
 }
