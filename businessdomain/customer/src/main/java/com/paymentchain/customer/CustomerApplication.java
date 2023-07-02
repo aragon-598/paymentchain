d9d9a24bf7aab7f0aee015df9ebcc32abc343944
Alejandro Aragon <aragondru155@gmail.com>
2023-07-01 20:42:40 -0600

    setting general name and adding configurations for entities

--- businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
@@ -3,6 +3,11 @@ package com.paymentchain.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,12 @@ public class CustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenApi(){
+		return new OpenAPI().components(new Components()).
+		info(new Info().title("Customer API").version("1.0.0"));
 	}
 
 }
