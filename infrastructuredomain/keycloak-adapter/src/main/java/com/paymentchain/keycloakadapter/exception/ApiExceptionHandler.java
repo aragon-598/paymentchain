1cc34ae115dbb8a5efed4f2229b49992dee250f0
Alejandro Aragon <aragondru155@gmail.com>
2023-07-06 15:02:50 -0600

    keycloak configuration

---
+++ infrastructuredomain/keycloak-adapter/src/main/java/com/paymentchain/keycloakadapter/exception/ApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.paymentchain.keycloakadapter.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<StandarizedApiExceptionResponse> handleBussinesRuleException(BusinessRuleException ex) {
+        StandarizedApiExceptionResponse response = new StandarizedApiExceptionResponse("Error de validacion",ex.getCode(),ex.getMessage());
+        return ResponseEntity.status(ex.getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandarizedApiExceptionResponse> handleNotContentException(Exception ex){
+        StandarizedApiExceptionResponse response = new StandarizedApiExceptionResponse("Error de validacion","Error-SPMC0001",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body(response);
+    }
+
+
+}
